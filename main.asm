51e1203f415cb56dc6907386725a597313b22e38
gytenis98 <c16gytenis.borusas.lt@usafa.edu>
2014-09-14 17:08:18 -0600

    corrected B functionality
    added comments and deleted command which i could not make to work. Got
    less lines of code which makes it
    "lighter"

--- main.asm
+++ main.asm
@@ -11,17 +11,16 @@
 				.cdecls C,LIST,"msp430.h"
 
 ;-------------------------------------------------------------------------------
-ADD_OP: 		.equ 	0x11
-SUB_OP: 		.equ 	0x22
-MUL_OP: 		.equ 	0x33
-CLR_OP: 		.equ 	0x44
-END_OP: 		.equ 	0x55
+ADD_OP: 		.equ 	0x11		;addition operation
+SUB_OP: 		.equ 	0x22		;subratrction operation
+CLR_OP: 		.equ 	0x44		;clear operation
+END_OP: 		.equ 	0x55		;halt operation
 OPERAND: 		.equ 	r6
 ACCUMULATOR:	.equ 	r5
 
 
-				.data
-Results: 		.space 	20
+				.data		;RAM
+Results: 		.space 	20	;space for answers
 
 
 				.text
@@ -37,67 +36,56 @@ StopWDT		mov.w 		#WDTPW|WDTHOLD,&WDTCTL
 ;-------------------------------------------------------------------------------
 		; check loop here
 CheckValues: 		.byte 			0x11, 0x11, 0x11, 0x11, 0x11, 0x44, 0x22, 0x22, 0x22, 0x11, 0xCC, 0x55 	; operation order
-
+;Here I add values for checking if program works.
 
 		mov.b 	#0, 			ACCUMULATOR
 		mov.b 	#1, 			OPERAND
 
 read
-		mov.b 	CheckValues, 		ACCUMULATOR
-		inc.b 	CheckValues
+		mov.b 	CheckValues, 		ACCUMULATOR  ;stores next value in accumulator
+		inc.b 	CheckValues						 ;increments checking value
 
 operand
-		mov.b 	CheckValues, 		OPERAND
-		inc.b 	CheckValues
-		cmp.b 	&ADD_OP, 		OPERAND
+		mov.b 	CheckValues, 		OPERAND		;moves value from my check command to operand location
+		inc.b 	CheckValues						;increments it
+		cmp.b 	&ADD_OP, 		OPERAND			;checks if it is addition
 		jmp 	addition
-		cmp.b 	&SUB_OP, 		OPERAND
+		cmp.b 	&SUB_OP, 		OPERAND			;checks if it is subraction
 		jmp 	subtract
-		cmp.b 	&MUL_OP, 		OPERAND
-		jmp 	multiply
-		cmp.b 	&CLR_OP, 		OPERAND
+		cmp.b 	&CLR_OP, 		OPERAND			;checks if it is clear command
 		jmp 	clear
-		cmp.b 	&END_OP,	 	OPERAND
+		cmp.b 	&END_OP,	 	OPERAND			;checks if it is halt command
 		jmp 	forever
 
-addition
+addition											;performes addition operation and stores the results
 		add.b 	CheckValues,	 	ACCUMULATOR
 		inc.b 	CheckValues
 		mov.b 	ACCUMULATOR, 	Results
 		inc.b 	CheckValues
-		jc 		overFlow
+		jc 		overFlow							;checks for overflow
 		jmp 	operand
 
-subtract
+subtract											;performes subtraction operation and stores the results
 		sub.b 	CheckValues,	 	ACCUMULATOR
 		inc.b 	CheckValues
 		mov.b	ACCUMULATOR,	Results
 		inc.b	Results
-		jc 		overFlow0
+		jc 		overFlow0							;checks for "0" overflow
 		jmp 	operand
 
-multiply
-		mov.b 	CheckValues, 		&MPY
-		inc.b 	CheckValues
-		mov.b 	ACCUMULATOR,	&OP2
-		NOP
-		mov.b 	#RESLO, 		ACCUMULATOR
-		jc 		overFlow
-		jmp 	operand
-
-clear
+clear												;performes clear operation and stores the results
 		mov.b 	#0, 			ACCUMULATOR
 		inc.b 	CheckValues
 		mov.b 	ACCUMULATOR, 	Results
 		inc.b 	Results
 		jmp 	operand
 
-overFlow
+overFlow											;checks for overflow
 		mov.b 	#255, 			ACCUMULATOR
 		jmp 	operand
 
-overFlow0
+overFlow0											;checks for "0" overflow
 		mov.b 	#0, 			ACCUMULATOR
 		jmp 	operand
 
-forever jmp 	forever
+forever jmp 	forever								;traps program in forever loop
